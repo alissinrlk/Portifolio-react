9cbb80860bf83d21fc00c7f826d7649495b1e7e5
Alisson <alissonvg42777@gmail.com>
2025-04-16 23:21:57 -0300

    alteracoes do header e footer

--- src/componentes/Footer.jsx
+++ src/componentes/Footer.jsx
@@ -5,7 +5,7 @@ export default function Footer() {
     return(
         <div>
             <footer>
-                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Velit a architecto delectus nihil pariatur libero atque adipisci laboriosam quasi voluptatum officia molestias officiis, itaque, omnis reiciendis? Saepe praesentium cumque ullam.</p>
+                <p>Desenvolvido por Alisson Alves.</p>
             </footer>
         </div>
     )
